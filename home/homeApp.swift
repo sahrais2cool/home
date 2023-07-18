e0328822a7019cf3d44f581f304b9cde11e65171
sahrais2cool <sahravann0@gmail.com>
2023-07-18 10:50:11 -0700

    Initial Commit

---
+++ home/homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeApp.swift
+//  home
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
